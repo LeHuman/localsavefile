9556b1eeb49a2e5122d232b4894ee9a25d47f9ce
LeHuman <47549331+LeHuman@users.noreply.github.com>
2024-08-01 16:53:07 -0500

    fix: :bug: Account for generics

--- localsavefile-derive/src/lib.rs
+++ localsavefile-derive/src/lib.rs
@@ -1,10 +1,13 @@
 extern crate proc_macro;
 
 use darling::{ast::NestedMeta, FromMeta};
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use quote::quote;
 use sanitize_filename::sanitize;
-use syn::{punctuated::Punctuated, token::Comma, Data, DeriveInput, Fields, Ident};
+use syn::{
+    punctuated::Punctuated, token::Comma, Data, DeriveInput, Fields, GenericParam, Generics, Ident,
+    ImplGenerics, WhereClause,
+};
 
 #[derive(Default, Debug, FromMeta)]
 struct LSFArgs {
@@ -16,9 +19,11 @@ struct LSFArgs {
 
 fn impl_default(
     input: &DeriveInput,
-    name: &Ident,
+    generics: &ImplGenerics,
+    name: &TokenStream,
+    extra_where: &TokenStream,
     impl_common: &proc_macro2::TokenStream,
-) -> TokenStream {
+) -> proc_macro::TokenStream {
     // TODO: Add Savefile onto attrs
     quote! {
         #[derive(::savefile::prelude::Savefile)]
@@ -26,16 +31,18 @@ fn impl_default(
 
         #impl_common
 
-        impl ::localsavefile::LocalSaveFile for #name {}
+        impl #generics ::localsavefile::LocalSaveFile for #name #extra_where {}
     }
     .into()
 }
 
 fn impl_persistent(
     input: &DeriveInput,
-    name: &Ident,
+    generics: &ImplGenerics,
+    name: &TokenStream,
+    extra_where: &TokenStream,
     impl_common: &proc_macro2::TokenStream,
-) -> TokenStream {
+) -> proc_macro::TokenStream {
     let attrs = &input.attrs;
 
     match &input.data {
@@ -87,7 +94,7 @@ fn impl_persistent(
 
                 #impl_common
 
-                impl ::localsavefile::LocalSaveFilePersistent for #name {
+                impl #generics ::localsavefile::LocalSaveFilePersistent for #name #extra_where{
                     fn get_file_handle_mut(&mut self) -> &mut Option<std::fs::File> {
                         &mut self.__place_localsavefile_above_any_derives.file
                     }
@@ -99,23 +106,56 @@ fn impl_persistent(
     .into()
 }
 
+// https://github.com/avl/savefile/blob/1cce218a9fce5ee328e6ed0c77e020e53ef8e8d5/savefile-derive/src/common.rs#L6
+pub(crate) fn get_extra_where_clauses(
+    gen2: &Generics,
+    where_clause: Option<&WhereClause>,
+    the_trait: TokenStream,
+) -> TokenStream {
+    let extra_where_separator;
+    if let Some(where_clause) = where_clause {
+        if where_clause.predicates.trailing_punct() {
+            extra_where_separator = quote!();
+        } else {
+            extra_where_separator = quote!(,);
+        }
+    } else {
+        extra_where_separator = quote!(where);
+    }
+    let mut where_clauses = vec![];
+    for param in gen2.params.iter() {
+        if let GenericParam::Type(t) = param {
+            let t_name = &t.ident;
+            let clause = quote! {#t_name : #the_trait};
+            where_clauses.push(clause);
+        }
+    }
+    let extra_where = quote! {
+        #extra_where_separator #(#where_clauses),*
+    };
+    extra_where
+}
+
 #[proc_macro_attribute]
-pub fn localsavefile(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn localsavefile(
+    args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let input: DeriveInput = syn::parse_macro_input!(input);
     let attr_args = match NestedMeta::parse_meta_list(args.into()) {
         Ok(v) => v,
         Err(e) => {
-            return TokenStream::from(darling::Error::from(e).write_errors());
+            return proc_macro::TokenStream::from(darling::Error::from(e).write_errors());
         }
     };
     let args = match LSFArgs::from_list(&attr_args) {
         Ok(v) => v,
         Err(e) => {
-            return TokenStream::from(e.write_errors());
+            return proc_macro::TokenStream::from(e.write_errors());
         }
     };
 
-    let name = &input.ident;
+    let name: &Ident = &input.ident;
     let name_str = sanitize(name.to_string());
     let version = args.version.unwrap_or(0);
     let path: Option<String> = args.path;
@@ -143,8 +183,17 @@ pub fn localsavefile(args: TokenStream, input: TokenStream) -> TokenStream {
         }
     };
 
+    let generics = &input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let extra_where = get_extra_where_clauses(
+        generics,
+        where_clause,
+        quote! {::savefile::prelude::Packed + ::std::default::Default + ::savefile::Serialize + ::savefile::Deserialize},
+    );
+    let for_name = quote! {#name #ty_generics #where_clause};
+
     let impl_common = quote! {
-        impl ::localsavefile::LocalSaveFileCommon for #name {
+        impl #impl_generics ::localsavefile::LocalSaveFileCommon for #for_name #extra_where {
             fn get_version() -> u32 {
                 #version
             }
@@ -172,8 +221,20 @@ pub fn localsavefile(args: TokenStream, input: TokenStream) -> TokenStream {
     };
 
     if persist {
-        impl_persistent(&input, name, &impl_common)
+        impl_persistent(
+            &input,
+            &impl_generics,
+            &for_name,
+            &extra_where,
+            &impl_common,
+        )
     } else {
-        impl_default(&input, name, &impl_common)
+        impl_default(
+            &input,
+            &impl_generics,
+            &for_name,
+            &extra_where,
+            &impl_common,
+        )
     }
 }
