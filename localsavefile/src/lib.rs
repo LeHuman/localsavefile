190fe90383a6d564fb7bf84245fd1a847c35784b
LeHuman <47549331+LeHuman@users.noreply.github.com>
2024-08-03 10:13:09 -0500

    feat: :sparkles: Add more implements for LocalSaveFileMetaData

--- localsavefile/src/lib.rs
+++ localsavefile/src/lib.rs
@@ -10,6 +10,7 @@ pub use crate::localsavefile::LocalSaveFile;
 pub use crate::localsavefilecommon::LocalSaveFileCommon;
 pub use crate::localsavefilepersistent::LocalSaveFilePersistent;
 pub use localsavefile_derive::localsavefile;
+pub use localsavefile_derive::localsavefile_impl;
 pub use sanitize_filename::sanitize;
 
 #[derive(Default, SavefileNoIntrospect, Debug)]
@@ -17,14 +18,23 @@ pub struct LocalSaveFileMetaData {
     #[savefile_ignore]
     pub file: Option<File>,
 }
+impl core::hash::Hash for LocalSaveFileMetaData {
+    fn hash<H: std::hash::Hasher>(&self, _state: &mut H) {}
+}
 impl PartialEq for LocalSaveFileMetaData {
     fn eq(&self, _other: &Self) -> bool {
         true
     }
 }
+impl Eq for LocalSaveFileMetaData {}
 impl PartialOrd for LocalSaveFileMetaData {
     fn partial_cmp(&self, _other: &Self) -> Option<std::cmp::Ordering> {
         Some(std::cmp::Ordering::Equal)
+    }
+}
+impl Ord for LocalSaveFileMetaData {
+    fn cmp(&self, _other: &Self) -> std::cmp::Ordering {
+        std::cmp::Ordering::Equal
     }
 }
 impl Clone for LocalSaveFileMetaData {
